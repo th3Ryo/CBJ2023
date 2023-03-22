e6fd0f90d5fa4593b1501f715f2fada04ef1f8b8
ryo <fabian_b.g@hotmail.com>
2023-03-22 16:17:02 -0500

    sexta clase

---
+++ coersion.js
@@ -0,0 +1,28 @@
+//coercion impicita
+
+var a=4+"7";
+typeof a
+
+//coercion impicita
+
+
+//coercion impicita
+
+var b=4*"7";
+typeof b
+
+//coercion impicita
+
+//coercion explicita
+var a = 2;
+var b = a +"";
+var c = String (a)
+typeof c
+//coercion explicita
+
+//coercion explicita 
+//se usa cada vez que el usuario digita un input
+
+var d = Number (c)
+typeof d
+//coercion explicita
