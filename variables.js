494209daba21826b2af79efe51ed73bc6946a460
ryo <fabian_b.g@hotmail.com>
2023-03-21 22:33:21 -0500

    segunda clase

---
+++ variables.js
@@ -0,0 +1,17 @@
+//declarar
+var nombre = "oscar";
+
+//declarar
+//inicializar
+var edad;
+edad=30;
+edad
+//inicializar
+
+var elementos = ["computador","celular"];
+
+
+var persona = {
+    nombre: "fabian",
+    edad: 30
+}
