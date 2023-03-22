5ecd29265c05a673577ce7ec5d1956b4fa1b161c
ryo <fabian_b.g@hotmail.com>
2023-03-21 22:49:17 -0500

    tercera clase

---
+++ funciones.js
@@ -0,0 +1,25 @@
+//declarativas
+function mifuncion () {
+    return 3
+}
+
+
+//declarativas
+//expresion
+
+var miFuncion =function (a,b){
+    return a+b;
+}
+
+miFuncion()
+//expresion
+
+function saludarEstudian(estudiante){
+    console.log(`hola ${estudiante}`);
+}
+
+function sumar(a,b){
+    return resultado
+    var resultado =a+b;
+}
+sumar(a,b)
