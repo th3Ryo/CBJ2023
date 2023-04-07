fd33609332c7588d74cfe42a2adbf126713600a9
ryo <fabian_b.g@hotmail.com>
2023-04-07 18:37:41 -0500

    doceaba  clase array

---
+++ arrays.js
@@ -0,0 +1,20 @@
+var frutas =["manzana","pera","fresa","limon"]
+
+console.log(frutas)
+
+//para saber la longitud
+console.log(frutas.length)
+
+//para una posicion especifica
+console.log(frutas[0])
+
+//METODOS modificar elementos array
+var frutas =["manzana","pera","fresa","limon"]
+
+var masFrutas = frutas.push("uvas"); //agregar contenido array ultimo
+var ultimoFrutas = frutas.pop("uvas"); // eliminar el ultimo
+var primerFrutas = frutas.unshift("uvas");// agregar contenido array primer
+var borrarmasFrutas = frutas.shit("uvas");// eliminar el primero
+var posicionmasFrutas = frutas.indexOf("fresa");
+
+
