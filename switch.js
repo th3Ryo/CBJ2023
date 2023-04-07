b29f11c6fabb35d1219ffab137d7c72dd42ceb84
ryo <fabian_b.g@hotmail.com>
2023-04-07 17:45:33 -0500

    onceaba  clase array

---
+++ switch.js
@@ -0,0 +1,14 @@
+var numero 1
+switch (numero) {
+    case 1:
+        console.log ("Soy uno");
+        break;
+    case 10:
+        console.log ("Soy UN DIEZ");
+        break;
+    case 100:
+        console.log ("Soy UN cien");
+        break;
+    default:
+        console.log("no soy ningun caso");
+}
